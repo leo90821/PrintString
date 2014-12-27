e06d7ef398111dc4e96081685bfaa754d808352f
NY <nycode.jn@gmail.com>
2014-12-27 21:01:21 +0800

    Initial Commit

---
+++ PrintString/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PrintString
+//
+//  Created by NiYao on 14/12/27.
+//  Copyright (c) 2014年 NiYao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
